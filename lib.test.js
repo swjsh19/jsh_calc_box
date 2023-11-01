4b04dcd8908b5e1defe744ca48bf0f52c1468e8b
seong hyeon Jeong <swjsh19@gmail.com>
2023-11-02 01:32:47 +0900

    complete

--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,26 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("fact([5]) should be 0", () => {
+    expect(lib.fact([5])).toBe(120);
+});
+
+test("fact([7]) should be 0", () => {
+    expect(lib.fact([7])).toBe(5040);
+});
+
+
+test("prime([7]) should be 0", () => {
+    expect(lib.prime([7])).toBe(true);
+});
+
+
+test("prime([1]) should be 0", () => {
+    expect(lib.prime([1])).toBe(false);
+});
+
+
+test("prime([2]) should be 0", () => {
+    expect(lib.prime([2])).toBe(true);
+});
